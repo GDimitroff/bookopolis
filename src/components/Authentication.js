d5b5f804bd51545045c6c8d2783961dbad7b7398
Gospodin Dimitrov <gospodindimitroff@gmail.com>
2022-10-13 14:11:15 +0300

    Refactor login form

--- src/components/Authentication.js
+++ src/components/Authentication.js
@@ -5,7 +5,7 @@ import Logo from './Logo';
 import { FcGoogle } from 'react-icons/fc';
 
 const Authentication = () => {
-  const [isLogin, setIsLogin] = useState(true);
+  const [showPassword, setShowPassword] = useState(false);
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -13,51 +13,49 @@ const Authentication = () => {
 
   return (
     <Wrapper>
-      <form onSubmit={handleSubmit} className="form">
-        <Logo showIcon={false} className="logo" />
-        {!isLogin && (
-          <div className="form-control">
-            <label htmlFor="username">Потребителско име</label>
-            <input
-              type="text"
-              placeholder="Иван"
-              name="username"
-              id="username"
-            />
-          </div>
-        )}
+      <form className="form" onSubmit={handleSubmit}>
+        <Logo className="logo" showIcon={false} />
         <div className="form-control">
           <label htmlFor="email">Имейл</label>
           <input
             type="email"
-            placeholder="ivan@books.com"
+            placeholder="ime@email.com"
             name="email"
             id="email"
           />
         </div>
         <div className="form-control">
           <label htmlFor="password">Парола</label>
-          <input type="password" name="password" id="password" />
+          <input
+            type={`${showPassword ? 'text' : 'password'}`}
+            name="password"
+            id="password"
+            minLength="6"
+            maxLength="25"
+          />
+          <button
+            className="toggle-password"
+            onClick={() =>
+              setShowPassword((prevShowPassword) => !prevShowPassword)
+            }>
+            {showPassword ? 'Скрий' : 'Покажи'}
+          </button>
         </div>
-        <button type="submit">{isLogin ? 'Вход' : 'Регистрирай се'}</button>
+        <button className="btn btn-submit" type="submit">
+          Вход / Регистрация
+        </button>
         <hr />
-        <button className="auth-with-google">
+        <button className="btn btn-google">
           <FcGoogle />
           <p>Влез с Google</p>
         </button>
       </form>
-      <p className="switch-mode">Нямаш профил? Регистрирай се!</p>
     </Wrapper>
   );
 };
 
 const Wrapper = styled.div`
-  .logo {
-    display: block;
-    text-align: center;
-    pointer-events: none;
-    margin: 2rem 0;
-  }
+  width: 55vw;
 
   .form {
     display: flex;
@@ -68,54 +66,50 @@ const Wrapper = styled.div`
     box-shadow: var(--light-shadow);
   }
 
+  .logo {
+    display: block;
+    text-align: center;
+    pointer-events: none;
+    margin: 2rem 0;
+  }
+
   .form-control {
+    position: relative;
     display: flex;
     flex-direction: column;
+
+    label {
+      font-size: 1.4rem;
+      margin-bottom: 0.6rem;
+    }
+
+    input {
+      font-family: inherit;
+      padding: 0.8rem 1.6rem;
+      border: 1px solid var(--color-grey-7);
+      outline: 1px solid transparent;
+      border-radius: var(--radius);
+      background-color: var(--color-grey-9);
+      transition: var(--transition);
+
+      &:focus {
+        outline: 1px solid var(--primary-brown);
+      }
+    }
+
+    .toggle-password {
+      position: absolute;
+      top: 34px;
+      right: 10px;
+      font-size: 1.2rem;
+    }
   }
 
-  .form-control label {
-    font-size: 1.4rem;
-    margin-bottom: 0.6rem;
-  }
-
-  .form-control input {
-    font-family: inherit;
-    padding: 0.8rem 1.6rem;
-    border: 1px solid var(--color-grey-7);
-    outline: 1px solid transparent;
-    border-radius: var(--radius);
-    background-color: var(--color-grey-9);
-    transition: var(--transition);
-  }
-
-  .form-control input:focus {
-    outline: 1px solid var(--primary-brown);
-  }
-
-  .switch-mode {
-    padding: 2rem;
-    margin-top: 3rem;
-    border-radius: var(--radius);
-    box-shadow: var(--light-shadow);
-  }
-
-  p {
-    font-size: 1.4rem;
-    text-align: center;
-  }
-
-  button {
-    font-size: 1.4rem;
-    font-family: inherit;
-    font-weight: 700;
-    border: none;
+  .btn-submit {
     color: var(--color-white);
     background-color: var(--primary-green);
     padding: 0.8rem 1.2rem;
     margin-top: 1rem;
-    border-radius: var(--radius);
-    transition: var(--transition);
-    cursor: pointer;
   }
 
   hr {
@@ -138,22 +132,18 @@ const Wrapper = styled.div`
     background: var(--primary-background-color);
   }
 
-  .auth-with-google {
+  .btn-google {
     display: flex;
     justify-content: center;
     align-items: center;
     gap: 6px;
+    padding: 0.8rem 1.2rem;
+    color: var(--color-grey-4);
     background-color: var(--color-grey-8);
-    color: var(--primary-brown);
-    margin-top: 0;
 
     svg {
       font-size: 2rem;
     }
-  }
-
-  .auth-with-google:hover {
-    background-color: var(--color-grey-9);
   }
 `;
 
