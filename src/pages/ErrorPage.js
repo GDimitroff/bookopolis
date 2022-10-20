760acea290a14d54d2890fe93aa58a49c50b13e3
Gospodin Dimitrov <gospodindimitroff@gmail.com>
2022-10-20 14:38:56 +0300

    Add error page

--- src/pages/ErrorPage.js
+++ src/pages/ErrorPage.js
@@ -1,5 +1,39 @@
+import styled from 'styled-components';
+
+import image from '../assets/undraw_location_search.svg';
+
 const ErrorPage = () => {
-  return <div>ErrorPage</div>;
+  return (
+    <Wrapper className="section">
+      <div className="image">
+        <img src={image} alt="" />
+      </div>
+      <h1>404</h1>
+      <p>Възникна грешка. Ресурсът, който търсите не е намерен.</p>
+    </Wrapper>
+  );
 };
+
+const Wrapper = styled.section`
+  min-height: calc(100vh - var(--navbar-height));
+  display: grid;
+  align-content: center;
+  place-items: center;
+
+  .image {
+    max-width: 500px;
+  }
+
+  img {
+    display: block;
+    width: 100%;
+  }
+
+  h1 {
+    font-size: 9.6rem;
+    margin-top: 2rem;
+    margin-bottom: 1rem;
+  }
+`;
 
 export default ErrorPage;
