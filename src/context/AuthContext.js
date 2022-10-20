2e5851f1f6194536c2370b5222e6f7998b4ee712
Gospodin Dimitrov <gospodindimitroff@gmail.com>
2022-10-20 10:36:52 +0300

    Refactor auth context

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,11 +1,5 @@
 import React, { useContext, useEffect, useReducer } from 'react';
-import {
-  collection,
-  getDocs,
-  setDoc,
-  updateDoc,
-  doc,
-} from 'firebase/firestore';
+import { collection, getDocs, setDoc, doc } from 'firebase/firestore';
 import {
   onAuthStateChanged,
   signInWithEmailAndPassword,
@@ -15,19 +9,12 @@ import {
   GoogleAuthProvider,
 } from 'firebase/auth';
 import { db, auth } from '../firebase';
-import {
-  ADD_BOOK_SUCCESS,
-  BOOK_LOADING,
-  USER_LOADING,
-  USER_SIGN_IN,
-  USER_SIGN_OUT,
-} from '../utils/actions';
+import { USER_LOADING, USER_SIGN_IN, USER_SIGN_OUT } from '../utils/actions';
 import autReducer from '../reducers/authReducer';
 
 const initialState = {
-  userLoading: true,
+  loading: true,
   user: null,
-  bookLoading: false,
 };
 
 const AuthContext = React.createContext();
@@ -52,20 +39,6 @@ const AuthProvider = ({ children }) => {
     signOut(auth);
   };
 
-  const addBook = async (book) => {
-    dispatch({ type: BOOK_LOADING });
-
-    try {
-      const userRef = doc(db, 'users', state.user.userId);
-      await updateDoc(userRef, {
-        addedBooks: [...state.user.addedBooks, book],
-      });
-      dispatch({ type: ADD_BOOK_SUCCESS, payload: book });
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, (user) => {
       dispatch({ type: USER_LOADING });
@@ -76,7 +49,7 @@ const AuthProvider = ({ children }) => {
         const fetchUser = async () => {
           const data = await getDocs(collection(db, 'users'));
           const users = data.docs.map((doc) => ({ ...doc.data() }));
-          const existingUser = users.find((user) => user.userId === id);
+          const existingUser = users.find((user) => user.id === id);
 
           if (existingUser)
             dispatch({
@@ -86,8 +59,8 @@ const AuthProvider = ({ children }) => {
           else {
             const userRef = doc(db, 'users', id);
             const newUser = {
-              userId: id,
-              email: email,
+              id,
+              email,
               addedBooks: [],
               favoriteBooks: [],
             };
@@ -117,7 +90,6 @@ const AuthProvider = ({ children }) => {
         signInWithGoogle,
         createUser,
         logout,
-        addBook,
       }}>
       {children}
     </AuthContext.Provider>
--- src/reducers/authReducer.js
+++ src/reducers/authReducer.js
@@ -1,55 +1,22 @@
-import {
-  ADD_BOOK_SUCCESS,
-  ADD_FAVORITE_BOOK_SUCCESS,
-  BOOK_LOADING,
-  USER_LOADING,
-  USER_SIGN_IN,
-  USER_SIGN_OUT,
-} from '../utils/actions';
+import { USER_LOADING, USER_SIGN_IN, USER_SIGN_OUT } from '../utils/actions';
 
 const autReducer = (state, action) => {
   switch (action.type) {
     case USER_LOADING: {
       return {
         ...state,
-        userLoading: true,
+        loading: true,
       };
     }
     case USER_SIGN_IN: {
       return {
         ...state,
-        userLoading: false,
+        loading: false,
         user: action.payload,
       };
     }
     case USER_SIGN_OUT: {
-      return { ...state, userLoading: false, user: null };
-    }
-    case BOOK_LOADING: {
-      return {
-        ...state,
-        bookLoading: true,
-      };
-    }
-    case ADD_BOOK_SUCCESS: {
-      return {
-        ...state,
-        bookLoading: false,
-        user: {
-          ...state.user,
-          addedBooks: [...state.user.addedBooks, action.payload],
-        },
-      };
-    }
-    case ADD_FAVORITE_BOOK_SUCCESS: {
-      return {
-        ...state,
-        bookLoading: false,
-        user: {
-          ...state.user,
-          favoriteBooks: [...state.user.favoriteBooks, action.payload],
-        },
-      };
+      return { ...state, loading: false, user: null };
     }
     default: {
       throw new Error(`No matching action type: "${action.type}"`);
